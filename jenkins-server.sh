05f03ecb24430a35a738b08e9a36a9692fa3a7fb
kunalb20 <136967201+kunalb20@users.noreply.github.com>
2024-04-26 15:21:27 +0530

    Create jenkins-server.sh

---
+++ jenkins-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+# this is Shebang script using /bin/bash to run commands
+
+################################################################################################
+# GIT Installation
+################################################################################################
+
+
+dnf install git-all
+apt install git-all
+git --version
